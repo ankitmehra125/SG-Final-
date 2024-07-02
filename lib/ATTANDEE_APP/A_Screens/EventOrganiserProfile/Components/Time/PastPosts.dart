2bccc91d95803ec35ef5a2e3fb93241b04ffb7fa
Ankit Mehra <ankii8946@gmail.com>
2024-07-02 17:09:31 +0530

    stubguys code updated

---
+++ lib/ATTANDEE_APP/A_Screens/EventOrganiserProfile/Components/Time/PastPosts.dart
@@ -0,0 +1,13 @@
+
+
+import '../../../BrowseEvents/Components/UpcomingEventsData.dart';
+
+final List<EventData> pastevents = [
+  EventData(
+    eventTitle: 'Techstartup Hub, Ikeja',
+    eventImage: 'Assets/Images/Components/browseeventsexample2.jpg',
+    eventDetails: 'StubGuys 2022 Meeting',
+    eventDate: 'June 23',
+  ),
+
+];
---
+++ lib/ATTANDEE_APP/A_Screens/EventOrganiserProfile/Components/Time/UpcomingPosts.dart
@@ -0,0 +1,18 @@
+
+
+import '../../../BrowseEvents/Components/UpcomingEventsData.dart';
+
+final List<EventData> upcomingevents = [
+  EventData(
+    eventTitle: 'Techstartup Hub, Ikeja',
+    eventImage: 'Assets/Images/Components/browseeventsexample1.jpg',
+    eventDetails: 'StubGuys Launch 2023',
+    eventDate: 'June 21',
+  ),
+  EventData(
+    eventTitle: 'Techstartup Hub, Ikeja',
+    eventImage: 'Assets/Images/Components/browseeventsexample2.jpg',
+    eventDetails: 'StubGuys VR Playground',
+    eventDate: 'June 21',
+  ),
+];
